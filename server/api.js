082a4090199bb22d175c2428666cf56a6e392e1b
Juan Burgos <burgos.135@gmail.com>
2024-09-17 12:58:09 -0600

    ideas Router Implemented

--- server/api.js
+++ server/api.js
@@ -2,7 +2,9 @@ const express = require('express');
 const apiRouter = express.Router();
 
 const minionRouter = require('./minionsRouter');
+const ideasRouter = require('./ideasRouter');
 
 apiRouter.use('/minions', minionRouter);
+apiRouter.use('/ideas', ideasRouter);
 
 module.exports = apiRouter;
---
+++ server/ideasRouter.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const ideasRouter = express.Router();
+
+const { createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase} = require('./db');
+
+    
+ideasRouter.get('/', (req, res, next) => {
+    console.log("fetching all ideas from database");
+    const ideasArray = getAllFromDatabase('ideas');
+    // console.log("total minion array "+minionArray.length);
+    res.status(200).send(ideasArray);
+});
+
+ideasRouter.get('/:id', (req, res, next) =>{
+    console.log("Fetching ideas by Id");
+    const idea = getFromDatabaseById('ideas', req.params.id);
+    if (idea === null){
+        res.status(404).send("Ideas Id not found");
+    } else{
+        res.send(idea);
+    }
+});
+
+ideasRouter.post('/', (req, res, next) => {
+    console.log("post idea");
+    const newIdea = addToDatabase('ideas', req.body)
+    if (newIdea === null){
+        res.status(400).send('Invalid Idea')
+    } else{
+        res.status(201).send(newIdea);
+    }
+});
+
+ideasRouter.put('/:id', (req, res, next)=>{
+    console.log("edit idea");
+    const idea = updateInstanceInDatabase('ideas', req.body);
+    if (idea === null){
+        res.status(404).send("Idea id not found");
+    } else{
+        res.send(idea);
+    }
+});
+
+ideasRouter.delete('/:id', (req,res,next) =>{
+    console.log("deleting one idea");
+    const status = deleteFromDatabasebyId('ideas', req.params.id)
+    if (status === true ){
+        res.status(204).send();
+    } else{
+        res.status(404).send("Idea id not found");
+    }
+})
+
+module.exports = ideasRouter;
